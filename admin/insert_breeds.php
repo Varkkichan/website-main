9bd14852c55a8b3a91514e5d0b8a7a4c934bbdbe
Varkkichan <ajayaji.383@gmail.com>
2024-03-13 10:34:13 +0530

    Updated breed id and validation

--- admin/insert_breeds.php
+++ admin/insert_breeds.php
@@ -2,12 +2,10 @@
     include('../includes/connect.php');
     if(isset($_POST['insert_breeds'])) {
         $breed_title = $_POST['breed_title'];
-
         // Check if the breed already exists
         $select_query = "SELECT * FROM `breeds` WHERE breed_title='$breed_title'";
         $result_select = mysqli_query($con, $select_query);
         $number = mysqli_num_rows($result_select);
-
         if($number > 0) {
             echo "<script>alert('This breed is already present in the database')</script>";
         } else {
@@ -30,11 +28,14 @@
 
 <h2 class="text-center">Insert Breeds</h2>
 <form action="" method="post" class="mb-2 w-50 m-auto">
+<div class="input-group mb-3 w-90 mb-2">
+        <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+        <input type="text" class="form-control" name="breed_id" placeholder="Enter Breed ID as 'catagorynamebreed' first letter of each word as capital" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
     <div class="input-group mb-3 w-90 mb-2">
         <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
         <input type="text" class="form-control" name="breed_title" placeholder="Insert Breed" aria-label="Username" aria-describedby="basic-addon1">
     </div>
-
     <div class="input-group w-10 mb-2 m-auto">
         <input type="submit" class="bg-info border-0 p-2 my-2" name="insert_breeds" value="Insert Breed" aria-label="Username" aria-describedby="basic-addon1"> 
     </div>
